ec8700ed4312fb568994df24c205e83c70691acb
snks0810 <89077247+snks0810@users.noreply.github.com>
2021-08-17 12:33:17 +0100

    Add files via upload

---
+++ Password checker/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String password = "password";
+	String passwordEntered = "not password";
+	if(passwordEntered == password){
+	    System.out.println("Correct Password");
+    }
+	else{
+        System.out.println("Incorrect Password");
+    }
+    }
+}
