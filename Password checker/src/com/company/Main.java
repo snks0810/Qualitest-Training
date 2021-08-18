3adb0077f78539b16f23aa99a7b245099067f7a9
snks0810 <89077247+snks0810@users.noreply.github.com>
2021-08-18 11:44:58 +0100

    Add files via upload
    Added a user entry feature for 5 possible passwords (still has 3 attempts per each one) using an array list. Also cleaned up any commented out code from previous iterations

--- Password checker/src/com/company/Main.java
+++ Password checker/src/com/company/Main.java
@@ -1,15 +1,19 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //String password = "password";
-        String[] passwords = {"password","not password","no","pa55w0rd"};
-        //boolean letters = false;
-        //boolean numbers = false;
+        ArrayList<String> passwords = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            System.out.println("Enter a password");
+            Scanner passwordInput = new Scanner(System.in);
+            String passwordEntered = passwordInput.nextLine();
+            passwords.add(passwordEntered);
+        }
+        System.out.println("");
         for(String i: passwords) {
             int length = i.length();
             int trials = 2;
@@ -18,26 +22,20 @@ public class Main {
                     System.out.println("Too short! Your password must be more than 3 characters");// short check
                 } else if (length > 10) {
                     System.out.println("Too long! Your password must be no longer than 10 characters");// long check
-                } else if (i.contains("0") == false && i.contains("1") == false && i.contains("2") == false && i.contains("3") == false &&
-                        i.contains("4") == false && i.contains("5") == false && i.contains("6") == false && i.contains("7") == false &&
-                        i.contains("8") == false && i.contains("9") == false) {
-                    System.out.println("Invalid! Password requires a number");
+                } else if (!i.contains("0")&& !i.contains("1")&& !i.contains("2") && !i.contains("3") && // poorly optimised
+                        !i.contains("4") && !i.contains("5") && !i.contains("6") && !i.contains("7") &&
+                        !i.contains("8") && !i.contains("9")) {
+                    System.out.println("Invalid! Password requires a number"); // number check
+                } else if (!i.contains("a") && !i.contains("b") && !i.contains("c") && !i.contains("d") && !i.contains("e") && !i.contains("f") && // poorly optimised
+                           !i.contains("g") && !i.contains("h") && !i.contains("i") && !i.contains("j") && !i.contains("k") && !i.contains("l") &&
+                           !i.contains("m") && !i.contains("n") && !i.contains("o") && !i.contains("p") && !i.contains("q") && !i.contains("r") &&
+                           !i.contains("s") && !i.contains("t") && !i.contains("u") && !i.contains("v") && !i.contains("w") && !i.contains("x") &&
+                           !i.contains("y") && !i.contains("z")){
+                    System.out.println("Invalid! Password requires a letter"); // letter check
                 } else {
                     System.out.println("Acceptable password");
                     break;
                 }
-
-                //for (int i = 0; i < length; i++) {
-                //    if (password.charAt(i) instanceof char == true)// letter check
-                //    {
-                //        letters = true;
-                //    } else if (password.charAt(i) instanceof int == true)// number check
-                //    {
-                //        numbers = true;
-                //    }
-                //}
-                //if (letters == false || numbers == false) {
-                //    System.out.println("Invalid, password must include both letters and numbers");
                 System.out.println("You have " + trials + " attempts remaining");
                 System.out.println("");
                 if (trials == 0) {
@@ -48,26 +46,6 @@ public class Main {
                 trials--;
             } while (trials >= 0);
         }
-
-            System.out.println("");
-            System.out.println("");
-
-            int counter = 1;
-            while (counter < 7) {
-                System.out.print(counter);
-                counter++;
-                System.out.println(" ");
-            }
-
-            System.out.println("");
-            System.out.println("");
-
-            int counterTwo = 1;
-            do {
-                System.out.println(counterTwo);
-                counterTwo++;
-            }
-            while (counterTwo < 7);
         }
     }
 
