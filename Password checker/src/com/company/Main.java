f76019723beb85ea6c35064657ae88cdae765f1b
snks0810 <89077247+snks0810@users.noreply.github.com>
2021-08-18 16:16:45 +0100

    Add files via upload
    Refined the letter and number check and put every check in a method. Fully working strength checker that terminates the programme when a strong password is entered.

--- Password checker/src/com/company/Main.java
+++ Password checker/src/com/company/Main.java
@@ -1,45 +1,90 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        ArrayList<String> passwords = new ArrayList<>();
-        for(int i = 0; i < 5; i++){
-            System.out.println("Enter a password");
-            Scanner passwordInput = new Scanner(System.in);
-            String passwordEntered = passwordInput.nextLine();
-            passwords.add(passwordEntered);
+    public static boolean letterCheck(String password) {
+        boolean hasLet = false;
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            if (password.toLowerCase().contains(String.valueOf(letter))) {
+                hasLet = true;
+                break;
+            }
         }
-        System.out.println("------------------------------------------------------------");
-        for(String i: passwords) {
-            int length = i.length();
-                if (length < 4) {
-                    System.out.println("Too short! Your password must be more than 3 characters");// short check
-                } else if (length > 10) {
-                    System.out.println("Too long! Your password must be no longer than 10 characters");// long check
-                } else if (!i.contains("0")&& !i.contains("1")&& !i.contains("2") && !i.contains("3") && // poorly optimised
-                        !i.contains("4") && !i.contains("5") && !i.contains("6") && !i.contains("7") &&
-                        !i.contains("8") && !i.contains("9")) {
-                    System.out.println("Invalid! Password requires a number"); // number check
-                } else if (!i.toLowerCase().contains("a") && !i.toLowerCase().contains("b") && !i.toLowerCase().contains("c") && // poorly optimised
-                           !i.toLowerCase().contains("d") && !i.toLowerCase().contains("e") && !i.toLowerCase().contains("f") &&
-                           !i.toLowerCase().contains("g") && !i.toLowerCase().contains("h") && !i.toLowerCase().contains("i") &&
-                           !i.toLowerCase().contains("j") && !i.toLowerCase().contains("k") && !i.toLowerCase().contains("l") &&
-                           !i.toLowerCase().contains("m") && !i.toLowerCase().contains("n") && !i.toLowerCase().contains("o") &&
-                           !i.toLowerCase().contains("p") && !i.toLowerCase().contains("q") && !i.toLowerCase().contains("r") &&
-                           !i.toLowerCase().contains("s") && !i.toLowerCase().contains("t") && !i.toLowerCase().contains("u") &&
-                           !i.toLowerCase().contains("v") && !i.toLowerCase().contains("w") && !i.toLowerCase().contains("x") &&
-                           !i.toLowerCase().contains("y") && !i.toLowerCase().contains("z")){
-                    System.out.println("Invalid! Password requires a letter"); // letter check
-                } else {
-                    System.out.println("Acceptable password");
-                    break;
-                }
+        return hasLet;
+    }
 
+    public static boolean numberCheck(String password) {
+        boolean hasNum = false;
+        for (int i = 0; i < 10; i++) {
+            if (password.contains(String.valueOf(i))) {
+                hasNum = true;
+                break;
+            }
         }
+        return hasNum;
+    }
+
+    public static int lengthCheck(String password) {
+        int outPut;
+        int length = password.length();
+        if (length <= 5) {
+            outPut = -1;
+        } else if (length <= 10) {
+            outPut = 0;
+        } else if (length <= 15) {
+            outPut = 1;
+        } else {
+            outPut = 2;
+        }
+        return outPut;
+    }
+
+    public static void strengthCalculator(int strengthResult) {
+        switch (strengthResult) {
+            case 0 -> {
+                System.out.println("This password is very weak");
+                System.out.println();}
+            case 1 -> {
+                System.out.println("This password is weak");
+                System.out.println();}
+            case 2 -> {
+                System.out.println("This password is ok");
+                System.out.println();}
+            case 3 -> {
+                System.out.println("This password is good");
+                System.out.println();}
+            case 4 -> {
+                System.out.println("This password is strong");
+                System.out.println();}
         }
     }
 
+    public static void main(String[] args) {
+
+        int strength = 0;
+        do {
+            strength = 0;
+            // User Input
+            System.out.println("Enter a password");
+            Scanner passwordInput = new Scanner(System.in);
+            String passwordEntered = passwordInput.nextLine();
+
+            // Length Checker
+            strength += lengthCheck(passwordEntered);
+
+            //Number Checker
+            if (numberCheck(passwordEntered)) {
+                strength++;}
+
+            //Letter Checker
+            if (letterCheck((passwordEntered))) {
+                strength++;}
+
+            //Strength Calculator
+            strengthCalculator(strength);
+        } while (strength < 4);
+        System.out.println("Password Accepted");
+    }
+}
